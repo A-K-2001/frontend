d67b75c3fb27ecb7149505d6c2afad5d98e5a642
A-K-2001 <kumaranilstps017@gmail.com>
2022-03-08 18:41:56 +0530

    add socket io

--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -6,19 +6,55 @@ import SendIcon from '@mui/icons-material/Send';
 import { Masg } from '../../components/Masg/Masg'
 import "./Chat.css"
 
+import { io } from "socket.io-client"
+
 const Chat = () => {
 
     const [conversation, setconversation] = useState([]);
 
     const [currentChat, setCurrentChat] = useState(null);
     const [message, setMessage] = useState([]);
+    const [arrivalMessage, setArrivalMessage] = useState(null);
     const [text, setText] = useState("");
+    const socket = useRef();
     const scrollref = useRef();
 
-    const user = useSelector(state => state.user.currentUser._id);
+    const user = useSelector(state => state.user?.currentUser._id);
 
     // console.log(user);
 
+    useEffect(() => {
+        socket.current = io("ws://localhost:8080");
+        socket.current.on("getMessage",(data)=>{
+
+            setArrivalMessage({
+                sender:data.senderId,
+                text: data.text,
+                createdAt:Date.now(),
+            });
+        });
+
+    }, []);
+
+    // console.log(currentChat.members);
+
+    useEffect(()=>{
+        arrivalMessage &&
+            currentChat?.members.includes(arrivalMessage.sender) &&
+            setMessage((perv)=>[...message, arrivalMessage]);
+    },[arrivalMessage,currentChat]);
+
+
+    // console.log(user);
+
+    useEffect(() => {
+        socket.current.emit("adduser",user);
+        socket.current.on("getUsers",users=>{
+            // console.log(users);
+        })
+    }, [user]);
+
+    
 
     useEffect(() => {
         const getconversation = async () => {
@@ -34,46 +70,56 @@ const Chat = () => {
 
 
 
-  
 
 
-    useEffect(()=>{
-            const getmassages = async()=>{
-                try{
-                    const res = await axios.get("/api/message/"+currentChat?._id);
-                    setMessage(res.data);
-                    // console.log(res);
-                }catch(err){
-                    console.log(err);
-                }
 
-            };
-            getmassages();
-    },[currentChat])
+    useEffect(() => {
+        const getmassages = async () => {
+            try {
+                const res = await axios.get("/api/message/" + currentChat?._id);
+                setMessage(res.data);
+                // console.log(res);
+            } catch (err) {
+                console.log(err);
+            }
+
+        };
+        getmassages();
+    }, [currentChat])
 
     // console.log(conversation._id);
     // // console.log(message);
-    
-    const handleClick = async(e)=>{
+
+    const handleClick = async (e) => {
 
         // e.preventDefault();
-        try{
-          
-            const res = await axios.post("/api/message/",{conversationId:currentChat?._id,text,sender:user});
-            setMessage([...message,res.data]);
-            setText("");
-            
 
-        }catch(err){
+
+        const receiverId = currentChat.members.find(member=>member!==user);
+        // console.log(receiverId);
+        socket.current.emit("sendMessage",{
+            senderId:user,
+            receiverId,
+            text:text,
+        });
+
+        try {
+
+            const res = await axios.post("/api/message/", { conversationId: currentChat?._id, text, sender: user });
+            setMessage([...message, res.data]);
+            setText("");
+
+
+        } catch (err) {
             console.log(err);
         };
-       
+
 
     };
 
-    useEffect(()=>{
-            scrollref.current?.scrollIntoView({behavior : "smooth"});
-    },[message]);
+    useEffect(() => {
+        scrollref.current?.scrollIntoView({ behavior: "smooth" });
+    }, [message]);
 
     return (
         <div class="container">
@@ -89,40 +135,40 @@ const Chat = () => {
                                 <div class="stylish-input-group">
                                     <input type="text" class="search-bar" placeholder="Search" />
                                     <span class="input-group-addon">
-                                        <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> 
+                                        <button type="button"> <i class="fa fa-search" aria-hidden="true"></i>
                                         </button>
                                     </span> </div>
                             </div>
                         </div>
                         <div class="inbox_chat">
-                            
+
                             {conversation.map((c) => (
-                                <div onClick={()=>setCurrentChat(c) } >
-                                <Inbox conversation={c} active={c?._id===currentChat?._id} />
+                                <div onClick={() => setCurrentChat(c)} >
+                                    <Inbox conversation={c} active={c?._id === currentChat?._id} />
                                 </div>
-                                
-                                ))}
+
+                            ))}
 
                         </div>
                     </div>
                     {currentChat ? <>
 
                         <div class="mesgs">
-                        <div class="msg_history">
-                            {
-                                message.map(m=>(
-                                    <div ref={scrollref}>
-                                    <Masg message={m} own ={m.sender=== user}/>
-                                    </div>
-                                ))
-                            }
+                            <div class="msg_history">
+                                {
+                                    message.map(m => (
+                                        <div ref={scrollref}>
+                                            <Masg message={m} own={m.sender === user} />
+                                        </div>
+                                    ))
+                                }
 
                             </div>
 
 
                             <div class="type_msg">
                                 <div class="input_msg_write">
-                                    <input type="text" class="write_msg" placeholder="Type a message" value={text} onChange={(e)=>setText(e.target.value)} />
+                                    <input type="text" class="write_msg" placeholder="Type a message" value={text} onChange={(e) => setText(e.target.value)} />
                                     <button class="msg_send_btn" type="button" onClick={handleClick}><SendIcon></SendIcon></button>
                                 </div>
                             </div>
